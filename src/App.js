f75454f32a35792dd48ea52bd2a185df396af49e
JermS <jermsama@gmail.com>
2024-10-01 22:01:21 -0400

    initial commit

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Hello JermS! You're awesome!
         </p>
         <a
           className="App-link"
